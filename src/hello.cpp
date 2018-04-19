6b52b003878298033caa67164e8dd93fde090e86
Andres Rodriguez <andresx7@gmail.com>
2018-04-19 15:30:45 -0400

    Initial meson hello project

---
+++ src/hello.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2018  Valve Corporation.
+ *
+ * This file is part of libvcetoy.
+ *
+ * libvcetoy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libvcetoy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libvcetoy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <vcetoy/hello.h>
+
+void say_hello()
+{
+    printf("Hello from libvcetoy\n");
+}
